9732d0666c088b49336156deac9071d9b5edb550
Kolyah35 <kolyah35@gmail.com>
2023-11-14 20:30:33 +0300

    fix logger for c++20

--- src/logger.hpp
+++ src/logger.hpp
@@ -7,18 +7,18 @@
 NS_SPECTRUM_BEGIN
 
 template <typename... Args>
-void logD(Args... args) {
-    fmt::print("[SPL] [I] {}\n", fmt::format(args...));
+void logD(fmt::format_string<Args...> s, Args&&... args) {
+    fmt::print("[SPL] [I] {}\n", fmt::format(s, std::forward<Args>(args)...));
 }
 
 template <typename... Args>
-void logW(Args... args) {
-    fmt::print(fmt::fg(fmt::terminal_color::bright_yellow), "[SPL] [W] {}\n", fmt::format(args...));
+void logW(fmt::format_string<Args...> s, Args&&... args) {
+    fmt::print(fmt::fg(fmt::terminal_color::bright_yellow), "[SPL] [W] {}\n", fmt::format(s, std::forward<Args>(args)...));
 }
 
 template <typename... Args>
-void logE(Args... args) {
-    fmt::print(fmt::fg(fmt::terminal_color::bright_red) | fmt::emphasis::bold, "[SPL] [E] {}\n", fmt::format(args...));
+void logE(fmt::format_string<Args...> s, Args&&... args) {
+    fmt::print(fmt::fg(fmt::terminal_color::bright_red) | fmt::emphasis::bold, "[SPL] [E] {}\n", fmt::format(s, std::forward<Args>(args)...));
 }
 
 NS_SPECTRUM_END
