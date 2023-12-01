b467a74563b04133150eb123ef621ec3a27cbc32
JaanDev <67502867+JaanDev@users.noreply.github.com>
2023-12-01 18:05:16 +0300

    a

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -20,8 +20,6 @@ Texture::Texture(const std::string& path) : m_format(0), m_size({0, 0}), m_textu
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
