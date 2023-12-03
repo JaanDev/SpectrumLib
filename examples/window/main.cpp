2f46a6f32e457a12f58aed4e1ce4a5f27ff68aab
JaanDev <67502867+JaanDev@users.noreply.github.com>
2023-12-03 12:30:33 +0300

    unicode

--- examples/window/main.cpp
+++ examples/window/main.cpp
@@ -1,117 +1,71 @@
 #include <Spectrum.hpp>
 
+#include <utf8.h>
+
 using namespace spl;
 #define create std::make_shared
-
-class MySprite : public Sprite {
-  public:
-    using Sprite::Sprite;
-
-    virtual void onMouseEvent(MouseEventType evtType, const Vec2f& mousePos) override {
-        if (evtType == MouseEventType::MouseEnterArea) {
-            printf("Enter area\n");
-            setColor({0, 255, 0});
-        } else if (evtType == MouseEventType::MouseLeaveArea) {
-            printf("Leave area\n");
-            setColor({255, 255, 255});
-        }
-    }
-
-    virtual void onKeyEvent(int key, int scancode, int action, int mods) override {
-        if (action == GLFW_PRESS) {
-            if (key == GLFW_KEY_LEFT) {
-                setPosX(getPosX() - 10);
-            } else if (key == GLFW_KEY_RIGHT) {
-                setPosX(getPosX() + 10);
-            }
-        }
-    };
-};
 
 class MyScene : public Scene {
   public:
     MyScene() : Scene() {
         printf("My scene created\n");
 
-        InputDispatcher::instance()->registerMouseEvents(this);
-        InputDispatcher::instance()->registerMouseScrollEvents(this);
-
-        m_spr = create<MySprite>("test.png");
-        m_spr->setPos(AppManager::instance()->getWinSize() / 2.f);
-        addChild(m_spr);
-
-        auto tm = TextureManager::instance();
-        tm->loadSpriteSheet("assets/test.json");
-        auto part1 = tm->getTextureFrame("part1.png");
-        auto part2 = tm->getTextureFrame("part2.png");
-
-        auto spr = create<Sprite>(part1);
-        spr->setPos({100, 100});
-        addChild(spr);
-
-        // works fine
-        spr->setTextureFrame(part2);
+        setBGColor({.1, .1, .1});
 
         auto fm = FontManager::instance();
-        fm->loadFont("C:\\Windows\\Fonts\\Arial.ttf", "pusab", 36, {FontRange::BasicLatin});
-        // fm->loadBitmapFont("pusab.fnt", "pusab");
+        fm->loadFont("C:\\Windows\\Fonts\\Arial.ttf", "arial", 36, {FontRange::BasicLatin, FontRange::Cyrillic});
+        fm->loadBitmapFont("pusab.fnt", "pusab");
 
-        auto fontSpr = create<Sprite>(fm->getFont("pusab").fontAtlas);
-        fontSpr->setPos({200, 200});
-        fontSpr->setAnchorPoint({0.5f, 0.0f});
-        fontSpr->setScale(0.25f);
-        addChild(fontSpr);
+        auto ttfSpr = create<Sprite>(fm->getFont("arial").fontAtlas);
+        ttfSpr->setPos({0, 0});
+        ttfSpr->setAnchorPoint({0, 0});
+        ttfSpr->setScale(120.f / ttfSpr->getTexture()->getSize().w);
+        addChild(ttfSpr);
 
-        std::string str = "Hello world!";
+        auto fntSpr = create<Sprite>(fm->getFont("pusab").fontAtlas);
+        fntSpr->setPos({400, 0});
+        fntSpr->setAnchorPoint({1, 0});
+        fntSpr->setScale(120.f / fntSpr->getTexture()->getSize().w);
+        addChild(fntSpr);
 
-        auto font = fm->getFont("pusab");
-        int posX = 20;
+        std::string str = "Привет Spectrum!";
 
-        for(char c : str) {
-            auto sprframe = std::make_shared<TextureFrame>(font.fontAtlas, font.glyphs[c].textureRect, false);
-            printf("%d %d %d %d %f %c\n", sprframe->getRect().x, sprframe->getRect().y, sprframe->getRect().w, sprframe->getRect().h, font.glyphs[c].yOffset, c);
-            auto ch = create<Sprite>(sprframe);
-            ch->setPosX(posX + font.glyphs[c].xOffset);
-            ch->setPosY(40 + font.glyphs[c].yOffset);
-            ch->setAnchorPoint({0, 0});
-            posX += font.glyphs[c].xAdvance;
-            addChild(ch);
-        }
+        float posY = 150.f;
+        for (const std::string& fontName : {"arial", "pusab"}) {
+            auto font = fm->getFont(fontName);
+            int posX = 0;
 
-        InputDispatcher::instance()->registerMouseEvents(m_spr.get());
-        InputDispatcher::instance()->registerKeyEvents(m_spr.get());
+            auto container = create<Node>();
 
-        setBGColor({.1, .1, .1});
-    }
-
-    virtual void onMouseBtn(int button, bool down, int mods) override {
-        if (down) {
-            if (button == GLFW_MOUSE_BUTTON_LEFT)
-                m_spr->setScale(m_spr->getScaleX() + 0.1f);
-            else if (button == GLFW_MOUSE_BUTTON_RIGHT)
-                m_spr->setRotation(m_spr->getRotation() + 5.f);
-            else if (button == GLFW_MOUSE_BUTTON_MIDDLE) {
-                m_spr->setScale(1.f);
-                m_spr->setPos(AppManager::instance()->getWinSize() / 2.f);
-                m_spr->setRotation(0.f);
+            auto it = str.begin();
+            auto end = str.end();
+            while (it != str.end()) {
+                auto cp = utf8::next(it, end);
+                const auto& glyph = font.glyphs[cp];
+                auto sprframe = std::make_shared<TextureFrame>(font.fontAtlas, glyph.textureRect, false);
+                auto ch = create<Sprite>(sprframe);
+                ch->setPosX(posX + glyph.xOffset);
+                ch->setPosY(glyph.yOffset);
+                ch->setAnchorPoint({0, 0});
+                posX += glyph.xAdvance;
+                container->addChild(ch);
             }
+
+            container->setPos({20, posY});
+            container->setScale(0.7f);
+            addChild(container);
+
+            posY += 30;
         }
     }
-
-    virtual void onMouseScroll(const Vec2f& delta) override {
-        //
-        m_spr->setPos(m_spr->getPos() + delta * 8.f);
-    };
-
-    // void update(float dt) override {}
 
   private:
-    std::shared_ptr<Sprite> m_spr;
+    //
 };
 
 int main() {
     FileManager::instance()->addSearchPath("assets");
-    WindowManager::instance()->createWindow({800, 600}, {400, 300}, "Hello Spectrum!", false,
+    WindowManager::instance()->createWindow({1024, 768}, {400, 300}, "Hello Spectrum!", false,
                                             WindowFlags::Default | WindowFlags::ScaleToMonitor | WindowFlags::Resizable);
 
     auto appMgr = AppManager::instance();
