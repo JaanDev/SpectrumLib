ab655e6e48d5bcbcf9803929269d683a7c89fa54
JaanDev <67502867+JaanDev@users.noreply.github.com>
2024-01-04 00:41:13 +0300

    use cpm for miniaudio

--- src/Scene.hpp
+++ src/Scene.hpp
@@ -9,7 +9,7 @@ class SPL_API Scene : public Node {
     Scene();
 
     inline void setBGColorF(const Col3f& color) { m_bgColor = color; }
-    inline void setBGColorU(const Col3u& color) { m_bgColor = utils::IntToCol3f(utils::Col3uToInt(color)); } // :skull:
+    inline void setBGColorU(const Col3u& color) { m_bgColor = {color.r / 255.f, color.g / 255.f, color.b / 255.f}; }
     inline const Col3f& getBGColor() const { return m_bgColor; }
 
   private:
